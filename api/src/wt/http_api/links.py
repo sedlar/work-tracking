340fc6fa195f2372964e0f19a3c8e2a46773cee0
Martin Sedlář <sedlar@mail.muni.cz>
2019-01-13 22:49:17 +0100

    Run link api in transaction

--- api/src/wt/http_api/links.py
+++ api/src/wt/http_api/links.py
@@ -1,4 +1,5 @@
 from flask_injector import inject
+import transaction
 
 from wt.http_api._common import handle_errors
 from wt.ids import EntityId
@@ -8,12 +9,14 @@ from wt.links import EntityLinksApi
 @inject
 @handle_errors
 def put_link(links_api: EntityLinksApi, object_id, other_object_id):
-    links_api.create_link(EntityId(object_id), EntityId(other_object_id))
+    with transaction.manager:
+        links_api.create_link(EntityId(object_id), EntityId(other_object_id))
     return {}, 201
 
 
 @inject
 @handle_errors
 def delete_link(links_api: EntityLinksApi, object_id, other_object_id):
-    links_api.delete_link(EntityId(object_id), EntityId(other_object_id))
+    with transaction.manager:
+        links_api.delete_link(EntityId(object_id), EntityId(other_object_id))
     return {}, 200
